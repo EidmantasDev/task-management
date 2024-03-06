7b36f1002a6425de73d536acc0f5f5ac24c1259f
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-06 16:02:09 +0200

    Init test Refactored code Wrote first tests Maybe in future add more tests?

---
+++ src/tasks/task.service.spec.ts
@@ -0,0 +1,63 @@
+import { TasksRepository } from './tasks.respository';
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TaskStatus } from './task-status.enum';
+import { NotFoundException } from '@nestjs/common';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someId',
+  password: 'somepassword',
+  tasks: [],
+};
+describe('Task Service', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('something');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('something');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls TasksRepository.findOne and returns the resut', async () => {
+      const mockTask = {
+        title: 'Test title',
+        description: 'Test desc',
+        id: 'someId',
+        status: TaskStatus.OPEN,
+      };
+
+      tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls TasksRepository.findOne and handles an error', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+});
