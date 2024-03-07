cf81204810971f289905fa01958a20a098257e5d
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-07 10:16:11 +0200

    Simplified connection to postgres Updated config validation schema Updated .env-example Fixed a mistake in getTasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,7 @@ import { configValidationSchema } from './config.schema';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT') || 5432,
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+        url: configService.get('DB_URI'),
         autoLoadEntities: true,
         synchronize: true,
       }),
